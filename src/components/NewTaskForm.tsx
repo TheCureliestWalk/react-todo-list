5a26b3e0645280b42656bf377a3d0b09fab3992a
Iho Somnam <ihosomnam4741@gmail.com>
2024-05-03 15:59:51 +0700

    update: tasks
    also decorate with Tailwind

--- src/components/NewTaskForm.tsx
+++ src/components/NewTaskForm.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+
 export default function NewTaskForm() {
+
+    const [task, setTask] = useState<{ id: number, title: string; description: string; }[]>([]);
+
+    const [taskTitle, setTaskTitle] = useState("");
+    const [taskDescription, setTaskDescription] = useState("");
+
+    const addTask = () => {
+        setTask([...task, { id: task.length + 1, title: taskTitle, description: taskDescription }]);
+        localStorage.setItem("tasks", JSON.stringify(task));
+        console.log(task);
+
+        // clear input fields
+        setTaskTitle("");
+        setTaskDescription("");
+    }
+
+    useEffect(() => {
+        const tasks = localStorage.getItem("tasks");
+        if (tasks) {
+            setTask(JSON.parse(tasks));
+        }
+    }, []);
+
     return (
         <div className="flex flex-col max-w-md mx-auto">
 
-        <form className="flex flex-col">
+        <div className="flex flex-col" >
           <div className="rounded border border-gray-300 shadow-lg">
           <input
             type="text"
@@ -10,19 +35,23 @@ export default function NewTaskForm() {
             name="taskTitle"
             className="w-full p-2.5 bg-white/70 border-b-2 border-b-sky-200"
             placeholder="I want to..."
+            value={taskTitle}
+            onChange={(e) => setTaskTitle(e.target.value)}
           />
           <textarea
             name="taskDescription"
             id="taskDescriptio"
             placeholder="a little detail here..."
             className="w-full p-2.5 bg-white/70 text-sm"
+            value={taskDescription}
+            onChange={(e) => setTaskDescription(e.target.value)}
           ></textarea>
           </div>
 
-          <button type="submit" className="w-full shadow-lg px-4 py-2.5 rounded border border-green-500 bg-green-500 hover:bg-green-700 duration-100 text-white">
+          <button className="w-full shadow-lg px-4 py-2.5 rounded border border-green-500 bg-green-500 hover:bg-green-700 duration-100 text-white" onClick={addTask}>
             Add Task
           </button>
-        </form>
+        </div>
     </div>
     )
 }
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+
 export default function Tasks() {
+
+    const [tasks, setTasks] = useState<{ id: number, title: string; description: string; }[]>([]);
+
+    useEffect(() => {
+        const tasks = localStorage.getItem("tasks");
+        if (tasks) {
+            setTasks(JSON.parse(tasks));
+        }
+    }, [])
+
     return (
-        <div className="mx-auto">
+        <div className="max-w-md mx-auto bg-white/70 text-gray-900 mt-8 p-2.5">
             <h1>All Tasks</h1>
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task.id} className="flex justify-between items-center py-2 hover:bg-green-300 duration-100 rounded">
+                        <div>
+                        <h3 className="font-bold text-base">{task.title}</h3>
+                        <p className="text-sm text-gray-700 ml-3 pl-1 border-l-2 border-sky-400">{task.description}</p>
+                        </div>
+
+                        <div>
+                            <button className="rounded bg-red-500 border border-red-400 px-4 text-white shadow-lg hover:bg-red-600 duration-100 font-bold text-sm">DELETE</button>
+                        </div>
+
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
